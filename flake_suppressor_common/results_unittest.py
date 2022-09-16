7b5715535623214332b439af2e5a41df6d0ca989
Preethi Mohan <preethim@google.com>
2022-09-16 18:52:54 +0000

    flake_suppressor tool: Generalize the Results module.
    Bug: 1358733
    
    Change-Id: Iddbcde7884540c7549d1bf01dab8472a52e68c72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3893117
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Preethi Mohan <preethim@google.com>
    Cr-Commit-Position: refs/heads/main@{#1048133}
    NOKEYCHECK=True
    GitOrigin-RevId: b6df8e92bcf86a77bb98e3b025fa5952aa4b5aae

--- flake_suppressor_common/results_unittest.py
+++ flake_suppressor_common/results_unittest.py
@@ -8,10 +8,12 @@
 import unittest
 import unittest.mock as mock
 
+from typing import List, Tuple
+
 from flake_suppressor_common import data_types
 from flake_suppressor_common import results
 from flake_suppressor_common import tag_utils as common_tag_utils
-from flake_suppressor import gpu_tag_utils as tag_utils
+from flake_suppressor_common import unittest_utils as uu
 
 GENERIC_EXPECTATION_FILE_CONTENTS = """\
 # tags: [ win ]
@@ -29,9 +31,8 @@ crbug.com/1111 [ win nvidia ] conformance/textures/misc/video-rotation.html [ Fa
 
 class BaseResultsUnittest(unittest.TestCase):
   def setUp(self) -> None:
-    # TODO(crbug.com/1358733): this will be removed when this module is
-    # generalized.
-    common_tag_utils.SetTagUtilsImplementation(tag_utils.GpuTagUtils)
+    common_tag_utils.SetTagUtilsImplementation(uu.UnitTestTagUtils)
+    self._results = uu.UnitTestResultProcessor()
     self._local_patcher = mock.patch(
         'flake_suppressor_common.results.expectations.'
         'GetExpectationFilesFromLocalCheckout')
@@ -105,74 +106,8 @@ class AggregateResultsUnittest(BaseResultsUnittest):
             },
         },
     }
-    self.assertEqual(results.AggregateResults(query_results), expected_output)
-
-  def testWithFiltering(self) -> None:
-    """Tests that results are properly filtered out."""
-    self._local_mock.return_value = {
-        'webgl_conformance_expectations.txt': GPU_EXPECTATION_FILE_CONTENTS,
-    }
-    query_results = [
-        # Expected to be removed.
-        {
-            'name': ('gpu_tests.webgl_conformance_integration_test.'
-                     'WebGLConformanceIntegrationTest.'
-                     'conformance/textures/misc/video-rotation.html'),
-            'id':
-            'build-1111',
-            'typ_tags': ['win', 'nvidia'],
-        },
-        # Expected to be removed.
-        {
-            'name': ('gpu_tests.webgl_conformance_integration_test.'
-                     'WebGLConformanceIntegrationTest.'
-                     'conformance/textures/misc/video-rotation.html'),
-            'id':
-            'build-2222',
-            'typ_tags': ['win', 'nvidia'],
-        },
-        {
-            'name': ('gpu_tests.webgl_conformance_integration_test.'
-                     'WebGLConformanceIntegrationTest.'
-                     'conformance/textures/misc/video-rotation.html'),
-            'id':
-            'build-3333',
-            'typ_tags': ['win', 'amd'],
-        },
-        {
-            'name': ('gpu_tests.webgl_conformance_integration_test.'
-                     'WebGLConformanceIntegrationTest.'
-                     'conformance/textures/misc/texture-npot-video.html'),
-            'id':
-            'build-4444',
-            'typ_tags': ['win', 'nvidia'],
-        },
-        {
-            'name': ('gpu_tests.pixel_integration_test.PixelIntegrationTest.'
-                     'Pixel_CSS3DBlueBox'),
-            'id':
-            'build-5555',
-            'typ_tags': ['win', 'nvidia'],
-        },
-    ]
-
-    expected_output = {
-        'webgl_conformance_integration_test': {
-            'conformance/textures/misc/video-rotation.html': {
-                ('amd', 'win'): ['http://ci.chromium.org/b/3333'],
-            },
-            'conformance/textures/misc/texture-npot-video.html': {
-                ('nvidia', 'win'): ['http://ci.chromium.org/b/4444'],
-            },
-        },
-        'pixel_integration_test': {
-            'Pixel_CSS3DBlueBox': {
-                ('nvidia', 'win'): ['http://ci.chromium.org/b/5555'],
-            },
-        },
-    }
-
-    self.assertEqual(results.AggregateResults(query_results), expected_output)
+    self.assertEqual(self._results.AggregateResults(query_results),
+                     expected_output)
 
 
 class ConvertJsonResultsToResultObjectsUnittest(BaseResultsUnittest):
@@ -209,38 +144,7 @@ class ConvertJsonResultsToResultObjectsUnittest(BaseResultsUnittest):
             '2222',
         ),
     ]
-    self.assertEqual(results._ConvertJsonResultsToResultObjects(r),
-                     expected_results)
-
-  def testDuplicateResults(self) -> None:
-    """Tests that duplicate results are not merged."""
-    r = [
-        {
-            'name': ('gpu_tests.webgl_conformance_integration_test.'
-                     'WebGLConformanceIntegrationTest.'
-                     'conformance/textures/misc/video-rotation.html'),
-            'id':
-            'build-1111',
-            'typ_tags': ['win', 'nvidia'],
-        },
-        {
-            'name': ('gpu_tests.webgl_conformance_integration_test.'
-                     'WebGLConformanceIntegrationTest.'
-                     'conformance/textures/misc/video-rotation.html'),
-            'id':
-            'build-1111',
-            'typ_tags': ['win', 'nvidia'],
-        },
-    ]
-    expected_results = [
-        data_types.Result('webgl_conformance_integration_test',
-                          'conformance/textures/misc/video-rotation.html',
-                          ('nvidia', 'win'), '1111'),
-        data_types.Result('webgl_conformance_integration_test',
-                          'conformance/textures/misc/video-rotation.html',
-                          ('nvidia', 'win'), '1111'),
-    ]
-    self.assertEqual(results._ConvertJsonResultsToResultObjects(r),
+    self.assertEqual(self._results._ConvertJsonResultsToResultObjects(r),
                      expected_results)
 
 
@@ -259,7 +163,7 @@ class FilterOutSuppressedResultsUnittest(BaseResultsUnittest):
         data_types.Result('bar_integration_test', 'foo_test', tuple(['win']),
                           'id')
     ]
-    self.assertEqual(results._FilterOutSuppressedResults(r), r)
+    self.assertEqual(self._results._FilterOutSuppressedResults(r), r)
 
   def testSuppressedResults(self) -> None:
     """Tests functionality when expectations apply to the given results."""
@@ -281,7 +185,7 @@ class FilterOutSuppressedResultsUnittest(BaseResultsUnittest):
                           'id'),
     ]
 
-    self.assertEqual(results._FilterOutSuppressedResults(r),
+    self.assertEqual(self._results._FilterOutSuppressedResults(r),
                      expected_filtered_results)
 
 
